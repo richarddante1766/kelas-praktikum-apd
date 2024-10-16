bc194ce7782b4507a16a03ecab021cd1627b9c55
richarddante1766 <138650898+richarddante1766@users.noreply.github.com>
2024-10-16 15:55:41 +0800

    pertemuan7

---
+++ Pertemuan7/2409106061_RichardDanteGunawan_C2_Pertemuan7.py
@@ -0,0 +1,66 @@
+# def mhs ():
+#     print ("Hello")
+
+# mhs ()
+
+# def kali ():
+#     x = 6*4
+#     print (x)
+    
+# kali ()
+
+# def luas_persegi_panjang (panjang, lebar):
+#     luas = panjang * lebar
+#     print ("Luas persegi panjang :", luas)
+
+# luas_persegi_panjang (4,6)
+
+# def luas_persegi (sisi):
+#     luas = sisi * sisi
+#     jumlah = sisi + sisi
+#     return jumlah, luas
+
+# luas, jumlah = luas_persegi (4)
+
+# print (luas)
+# print (jumlah)
+
+# def mhs (nama, nim, **arr):
+#     print (nama)
+#     print (nim)
+#     print (arr)
+    
+# mhs ("Ucup", '20', data = 2, data = 3)
+
+# def nilai (angka):
+#     if angka > 10:
+#         print ("1")
+#         return
+#     else:
+#         print ("2")
+#         return
+# print ("3")
+    
+# nilai (10)
+
+
+# data  =10
+
+# def nilai ():
+#     data = 20
+#     print (data)
+    
+# print (data)
+
+# def data (kumpulan):
+#     print (kumpulan)
+    
+# value = [1,2,3,4]
+# data (value)
+
+# def mhs(nama1, nama2, nama3):
+#     print (nama3)
+
+# mhs (nama3 = "ucup", nama2 = "saipul", nama1 = "michael")
+
+
